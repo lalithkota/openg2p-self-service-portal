63bef1c406ee0cfa8e122a2d64c476c8e0ee4d68
Lalith Kota <kotalalith@gmail.com>
2023-01-31 21:22:14 +0530

    Reorganized Templates

--- g2p_ssp_dashboard/controllers/login.py
+++ g2p_ssp_dashboard/controllers/login.py
@@ -3,7 +3,7 @@ import logging
 from odoo import http
 from odoo.http import request
 
-from odoo.addons.auth_oauth.controllers.main import OAuthController, OAuthLogin, fragment_to_query_string
+from odoo.addons.auth_oauth.controllers import main as auth_oauth
 
 _logger = logging.getLogger(__name__)
 
@@ -12,17 +12,22 @@ class SSPLoginContorller(http.Controller):
     @http.route(["/ssp/login"], type="http", auth="public")
     def ssp_login(self, **kwargs):
         request.params["redirect"] = "/"
+        context = {}
 
-        providers = [
-            p for p in OAuthLogin().list_providers() if p.get("g2p_ssp_allowed", False)
-        ]
-        return request.render(
-            "g2p_ssp_dashboard.g2p_ssp_login_page", {"providers": providers}
+        context.update(
+            dict(
+                providers=[
+                    p
+                    for p in auth_oauth.OAuthLogin().list_providers()
+                    if p.get("g2p_ssp_allowed", False)
+                ]
+            )
         )
+        return request.render("g2p_ssp_dashboard.g2p_ssp_login_page", qcontext=context)
 
 
-class SSPSigninController(OAuthController):
+class SSPSigninController(auth_oauth.OAuthController):
     @http.route("/ssp/signin", type="http", auth="none")
-    @fragment_to_query_string
+    @auth_oauth.fragment_to_query_string
     def signin(self, **kw):
         return super(SSPSigninController, self).signin(**kw)
